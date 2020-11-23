ed2e84a2007db73ff984db230f2f429cd3ab1680
nakajima-ittoku <nakajima.web.tips@gmail.com>
2020-11-23 15:23:54 +0900

    update: todolist

--- src/Todo/TodoList.jsx
+++ src/Todo/TodoList.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import DeleteBtn from './DeleteBtn.jsx'
 
 // ここでは、addFormで取得したTODOのリスト内容を追加していく
 class TodoList extends Component {
+
     render() {
-        let Todo = this.props.data.map((value) => (
-            <li key={value.message}>{value.message}</li>
+        let Todo = this.props.data.map((value, index) => (
+            <li key={index}>
+                <div>{value.message}</div>
+                <DeleteBtn index={index} />
+            </li>
         ))
 
         return (
@@ -16,6 +21,4 @@ class TodoList extends Component {
     }
 }
 
-
-// Storeの内容を使うので、まずはconnect
 export default connect((state) => state) (TodoList)
