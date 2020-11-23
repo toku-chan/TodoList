386817e1d2eb0f1220c5c3c1f084169a8155cef7
nakajima-ittoku <nakajima.web.tips@gmail.com>
2020-11-23 10:08:50 +0900

    fix: 不要なコメントアウトを削除

--- src/Todo/TodoList.jsx
+++ src/Todo/TodoList.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 
 // ここでは、addFormで取得したTODOのリスト内容を追加していく
 class TodoList extends Component {
-    // Storeのdate配列として、messageが格納されているので、それを特定のタグで展開すれば良いと考えている
     render() {
         let Todo = this.props.data.map((value) => (
             <li key={value.message}>{value.message}</li>
