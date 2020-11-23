1b707823ddca0d2e4351b32b77b0f7922d2acbbc
nakajima-ittoku <nakajima.web.tips@gmail.com>
2020-11-23 15:23:35 +0900

    update: store

--- src/Todo/Store.jsx
+++ src/Todo/Store.jsx
@@ -11,11 +11,15 @@ export function todoReducer(state = init, action) {
         case 'ADD':
             return addReducer(state, action)
 
+        case 'DELETE':
+            return deleteReducer(state, action)
+
         default:
             return state
     }
 }
 
+// 追加処理
 function addReducer(state, action) {
     let data = {
         message: action.message
@@ -28,10 +32,13 @@ function addReducer(state, action) {
     }
 }
 
-export function addMemo(text) {
+// 削除処理
+function deleteReducer(state, action) {
+    let newdata = state.data.slice()
+    newdata.splice(action.index, 1)
+
     return {
-        type: 'ADD',
-        message: text
+        data: newdata
     }
 }
 
